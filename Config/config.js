381d334146fd37e32a2b939b5afe82286e9c2ee3
yasir095 <yasir.se@gmail.com>
2016-02-05 11:24:13 +0000

    new api calls added, error logging supported

--- Config/config.js
+++ Config/config.js
@@ -6,25 +6,33 @@ var environment = "development";
 switch(environment) {
 
     case "development":
-        var authServerUrl = "http://secure.merchantstack.com";
-        var apiServerUrl = "http://api.merchantstack.com";
+        var oAuthBaseUrl = "http://secure.merchantstack.com";
+        var apiBaseUrl   = "http://api.merchantstack.com";
+        var authUri      = "/oauth/authorize";
+        var tokenUri     = "/oauth/token";
+        var logoutUri    = "/logout";
         break;
     case "staging":
 
         break;
     case "production":
-        var authServerUrl = "http://secure.merchantstack.com";
-        var apiServerUrl = "http://api.merchantstack.com";
+        var oAuthBaseUrl = "http://secure.merchantstack.com";
+        var apiBaseUrl   = "http://api.merchantstack.com";
+        var authUri      = "/oauth/authorize";
+        var tokenUri     = "/oauth/token";
+        var logoutUri    = "/logout";
         break;
 }
 
 module.exports = {
 
-    authServerUrl       : authServerUrl,
-    apiServerUrl        : apiServerUrl,
+    oAuthBaseUrl       : oAuthBaseUrl,
+    apiBaseUrl        : apiBaseUrl,
+    apiVersion          : "0.6", //supply your api version in ConnectionConfig.options
 
     accessType          : "online",
     defaultScope        : "application",
     responseType        : "code",
-    acceptEncoding      : null          //possible option "gzip, inflate"
+    acceptEncoding      : null,          //possible option "gzip, inflate"
+    displayErrorLog     : true
 };
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Created by yasirmahmood on 27/01/2014.
  */
 var ConnectionConfig = require('./lib/ConnectionConfig'),
-    Auth   = require('./lib/Auth');
+    Auth   = require('./lib/ShopwaveConnectManager');
 
 exports.createConnection = function(config) {
     return new Auth({config: new ConnectionConfig(config)});
